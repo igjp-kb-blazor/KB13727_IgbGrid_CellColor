a16d0f9f91493b65e9325c236b1af325884e5250
TomoyaWashio <148823993+TomoyaWashio@users.noreply.github.com>
2024-06-18 15:51:27 +0900

    初回コミット

---
+++ wwwroot/scrip.js
@@ -0,0 +1,21 @@
+﻿/*氏名 列の条件付きクラスを設定*/
+igRegisterScript("CellClassesHandler1", function () {
+    return {
+        person: (rowData, columnKey) => rowData[columnKey] === "Bob",
+    };
+}, true);
+
+/*年齢 列の条件付きクラスを設定*/
+igRegisterScript("CellClassesHandler2", () => {
+    return {
+        upFont: (rowData, columnKey) => rowData[columnKey] >= 35,
+        downFont: (rowData, columnKey) => rowData[columnKey] < 35
+    };
+}, true);
+
+/*職業 列の条件付きクラスを設定*/
+igRegisterScript("CellClassesHandler3", function () {
+    return {
+        "job-title-cell": (rowData, columnKey, cellValue, rowIndex) => true
+    };
+}, true);
